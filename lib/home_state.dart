7d339d1721f6a7c0d5cccd53daba3dd89f45d7fb
hieuvm <hieuvm.nws@gmail.com>
2024-01-19 14:39:22 +0700

    finish

--- lib/home_state.dart
+++ lib/home_state.dart
@@ -2,26 +2,21 @@ part of 'home_cubit.dart';
 
 class HomeState extends Equatable {
   final int curlIndexDot;
-  final bool isActive;
 
   @override
   List<Object?> get props => [
         curlIndexDot,
-        isActive,
       ];
 
   const HomeState({
     this.curlIndexDot = 0,
-    this.isActive = false,
   });
 
   HomeState copyWith({
     int? curlIndexDot,
-    bool? isActive,
   }) {
     return HomeState(
       curlIndexDot: curlIndexDot ?? this.curlIndexDot,
-      isActive: isActive ?? this.isActive,
     );
   }
 }
